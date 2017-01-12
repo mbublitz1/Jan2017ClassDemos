e414cfd25ab03d77a21754e21fff7813e4a38db1
mbublitz1 <mbublitz1@nait.ca>
2017-01-11 18:16:03 -0700

    Completion of Entity framework example

---
+++ ClassDemo/Chinook.System/BLL/AlbumController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using Chinook.System.DAL;
+using System.ComponentModel;
+using Chinook.Data.POCOs;
+#endregion
+
+namespace Chinook.System.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<AlbumArtist> ListAlbumsByArtist()
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Albums
+                              orderby x.Artist.Name
+                              select new AlbumArtist
+                              {
+                                  Artist = x.Artist.Name,
+                                  Title = x.Title,
+                                  ReleaseYear = x.ReleaseYear,
+                                  ReleaseLabel = x.ReleaseLabel
+                              };
+                return results.ToList();
+            }
+        }
+    }
+}
